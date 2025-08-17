e885ef6f3223aa26b477ece6511d8bc5492cf27b
Dmytro Gutorka <dgutorka@gmail.com>
2025-08-17 19:11:27 +0300

    Add SQL query fir fetching overall runtime for tv show and movies

--- backend/src/services/users.service.ts
+++ backend/src/services/users.service.ts
@@ -14,39 +14,55 @@ export const usersService = new UsersService();
 
 // AVG/MAX/MIN rating:
 // SELECT
-//   COUNT(*) AS number_of_ratings,
-//   AVG(rating) AS avg_rating,
-//   MAX(rating) AS max_rating,
-//   MIN(rating) AS min_rating
+//     COUNT(*) AS number_of_ratings,
+//     AVG(rating) AS avg_rating,
+//     MAX(rating) AS max_rating,
+//     MIN(rating) AS min_rating
 // FROM
-// "media_user_action"
+//     "media_user_action"
 // WHERE
-// "userId" = 1
+//     "userId" = 1
 
 // TOP 10 rated movies/tv show
 // SELECT
-//     "mediaInfoId",
-//     "rating",
-//     "mediaType"
+//    "mediaInfoId",
+//    "rating",
+//    "mediaType"
 // FROM
-//      "media_user_action"
+//    "media_user_action"
 // WHERE
 //     "userId" = 1 AND "mediaType" = 'movie' / 'tv'
 // ORDER BY
-//     "rating" DESC
+//    "rating" DESC
 // LIMIT
-//     10;
+//    10;
 
 // Number of comments
+// SELECT
+//    COUNT(*)
+// FROM
+//    "comments"
+// WHERE
+//    "userId" = 1
+
+// Overall MOVIES runtime
+// Overall TV runtime
+// SELECT
+//    AVG(runtime)
+// FROM
+//    "media_user_action" as mua
+// LEFT JOIN
+//    "media_info" as mi
+// ON
+//    "mi"."id" = "mua"."mediaInfoId"
+// WHERE
+//    "userId" = 1 AND "mi"."mediaType" = 'movie';
 
 // Favorites genres 3-5
 // Number of watched EPISODES
 
 // Number of watched MOVIES
 // Number of watched TV
-
-// Overall MOVIES runtime
-// Overall TV runtime
 
 // ?? Times Rewatched
 // ?? Review likes received
